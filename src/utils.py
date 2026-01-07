3b706551e60aa1f79a459ccf204c550f771a52d4
cadenpascual <capascual@ucsd.edu>
2026-01-07 15:58:03 -0800

    added a few datasets

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
+import xarray as xr        
 
-# JSON -> Tensor (4D array: Time x Players x (x,y))
+# JSON -> Tensor (3D array: Moment, Players, (x,y) Coords)
 def event_to_tensor(event, max_frames=None):
     moments = event["moments"]
     if max_frames:
@@ -16,5 +17,34 @@ def event_to_tensor(event, max_frames=None):
             traj[t, i, 1] = p[3]  # y
 
     return traj
+
+
+def build_dataset(game, max_frames=150):
+    plays = []
+    for event in game["events"]:
+        if "moments" not in event or len(event["moments"]) < 10:
+            continue
+        plays.append(event_to_tensor(event, max_frames))
+
+    P = len(plays)
+    T = max(p.shape[0] for p in plays)
+
+    data = np.zeros((P, T, 10, 2))
+
+    for i, p in enumerate(plays):
+        data[i, :p.shape[0]] = p
+
+    return xr.Dataset(
+        data_vars={
+            "positions": (["play", "time", "player", "coord"], data)
+        },
+        coords={
+            "play": np.arange(P),
+            "time": np.arange(T),
+            "player": np.arange(10),
+            "coord": ["x", "y"]
+        }
+    )
+
 
 
